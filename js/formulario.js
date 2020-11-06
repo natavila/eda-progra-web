c8a5659565a13dbcf7ce284ac6194acb0ea0829a
natavila <agustina_avila12@hotmail.com>
2020-11-05 22:41:04 -0300

    modified:   contacto.html 	modified:   estilos/contacto.css 	modified:   formulario.html 	new file:   js/contacto.js 	new file:   js/formulario.js

---
+++ js/formulario.js
@@ -0,0 +1,37 @@
+function mostrarPaises(){
+    const menuPaises=document.querySelector(".paisesVisitados");
+    menuPaises.removeAttribute("hidden");
+}
+
+function ocultarPaises(){
+    const menuPaises=document.querySelector(".paisesVisitados");
+    menuPaises.setAttribute("hidden", "true");
+}
+
+function mostrarDireccion(){
+    const menuDireccion=document.querySelector(".direccion");
+    menuDireccion.removeAttribute("hidden");
+}
+
+function ocultarDireccion(){
+    const menuDireccion=document.querySelector(".direccion");
+    menuDireccion.setAttribute("hidden", "true");
+}
+
+
+
+function mostrarSintomas(){
+    let opcioneSi= document.getElementsByClassName("opcionSi");
+    let contador= 0;
+
+    for (let i = 0; i < opcioneSi.length; i++) {
+        let valor=opcioneSi[i].value;
+        if (valor == "si" && opcioneSi[i].checked) {
+            contador++;
+        }
+        
+    }
+   
+    let mensaje=`El formulario fue completado correctamente. ${contador} síntomas de COVID-19 fueron registrados`;
+    alert(mensaje);
+}
