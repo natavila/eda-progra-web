0da84e15b8c644671bebfd688b5f27ca442c0e31
Esteban Barone <ebarone@alumno.unlam.edu.ar>
2020-11-06 07:59:41 -0300

    formulario terminado

--- js/formulario.js
+++ js/formulario.js
@@ -18,8 +18,6 @@ function ocultarDireccion(){
     menuDireccion.setAttribute("hidden", "true");
 }
 
-
-
 function mostrarSintomas(){
     let opcioneSi= document.getElementsByClassName("opcionSi");
     let contador= 0;
@@ -34,4 +32,47 @@ function mostrarSintomas(){
    
     let mensaje=`El formulario fue completado correctamente. ${contador} síntomas de COVID-19 fueron registrados`;
     alert(mensaje);
+}
+
+function validarCampos(){
+    let var1=validacionNombre();
+    let var2=validarTelefono();
+
+    if(var2){
+        alert(`datos cargados correctamente!!`);
+    }
+}
+
+function validacionNombre(){
+    let elements=document.getElementById("nya");
+
+    let c=elements.getAttribute("required");
+    
+    if (c == null) {
+        elements.setAttribute("required", "true");
+    }
+    
+}
+
+function validarTelefono(){
+    let apto=false;
+
+    var telefono=document.querySelector(".validarTelefono").value;
+    let separador= "";
+
+    telefono = telefono.split(separador);
+
+    let checkGuion=false;
+
+    if(telefono[4] == "-"){
+          checkGuion=true;         
+    }      
+   
+    if (telefono.length != 9 || !checkGuion) {
+        alert("El telefono debe contener 8 caracteres y un Guion");
+    }else{
+        apto=true;
+    }
+
+    return apto;
 }
