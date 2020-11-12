ebe1c4cfa9342f15146d5c736f4e516037643dde
estyCai32 <71356054+estyCai32@users.noreply.github.com>
2020-11-12 16:55:48 -0300

    terminado RESPONSIVE de: index,formulario

---
+++ slider.js
@@ -0,0 +1,19 @@
+// jqDocReady para ejecutar esto mas rapido
+//SELECTORES= JQUERY $('.slider h1')   .html (EL .html inserta dentro de la etiqueta seleccionada lo que le pasamos por parametro)
+//                    (elemPadre dentroDelElempadre:first/last) 
+
+//MANIPULAR DOM
+  //addClass(`nombreclas`);     // append('<h1>Titulo</h1>');
+  //removeClass(`nombreclas`);  // css()
+  //.remove() .hide()
+  // .attr('attributeName', 'value');  ---> Setear atributos
+  // $(selector).attr('attributeName'); ---> get atributos 
+
+//EVENTOS
+//
+
+$(document).ready(function(){
+       
+
+});
+
